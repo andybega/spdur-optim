8f01f01976cdfc962ba43b880cf27946fc476dcc
hermes829 <hermes829@gmail.com>
2015-03-01 23:34:29 -0500

    simple cleanup

--- ilc-example/runSpLogLog.R
+++ ilc-example/runSpLogLog.R
@@ -15,8 +15,11 @@ packs=c("corpcor", 'microbenchmark', 'Rcpp', 'RcppArmadillo', 'ggplot2')
 loadPkg(packs)
 
 # Load core estimation functions
-core_funcs = list.files("../core-functions", pattern="*.R")
-for(func in core_funcs){source(paste0('../core-functions/', func))}
+coreFuncs = list.files("../core-functions", pattern="*.R")
+for(func in coreFuncs){source(paste0('../core-functions/', func))}
+
+cppFuncs = list.files("../core-functions", pattern="*.cpp")
+for(func in cppFuncs){sourceCpp(paste0('../core-functions/', func))}
 
 # Look only at likelihood calculation
 # (Y, X, Z saved from debugging spdur)
@@ -27,66 +30,12 @@ theta <- rnorm(ncol(X) + ncol(Z) + 1)
 
 ################################################################
 # Make sure cpp versions return same result
-sourceCpp("../core-functions/loglog_lnlC.cpp")
 loglog_lnlCpp(theta, Y, X); loglog_lnl(theta, Y, X)
-
-sourceCpp("../core-functions/sploglog_lnlC.cpp")
 sploglog_lnlCpp(theta, Y, X, Z); sploglog_lnl(theta, Y, X, Z)
 ################################################################
 
 ################################################################
 # Speed comparison
-# First we pass cpp likelihood into existing functions from sploglog.R
-#' Regular Log-logistic regression
-loglogCpp <- function(Y, X, inits=NULL, max.iter, silent=TRUE) {
-  if (is.null(inits)) {
-    inits <- c(rep(0, ncol(X)), 0)
-  }
-  
-  trace <- !silent
-  est <- optim(inits, loglog_lnlCpp, method="BFGS", 
-               control=list(trace=trace, maxit=max.iter), 
-               hessian=T, y=Y, X=X)
-  
-  # Solve other results
-  if (est$convergence!=0 & !silent) stop('Model did not converge')
-  coef <- est$par
-  vcv <- solve(est$hessian)
-  vcv <- make.positive.definite(vcv)
-  logL <- -est$value
-  
-  # Put together results
-  return(list(coefficients = coef, vcv = vcv, logL = logL))
-}
-
-# Split Population
-sploglogCpp <- function(Y, X, Z, max.iter, silent=FALSE) {
-  # Estimate base model
-  if (!exists("base.inits")) {
-    base.inits <- c(rep(0, ncol(X)), 1)
-  }
-  if (!silent) cat('Fitting base loglog...\n')
-  base <- loglog(Y=Y, X=X, inits=base.inits, max.iter=200, silent=TRUE)
-    
-  # Estimate full model
-  x.inits <- base$coefficients[1:ncol(X)]
-  a.init <- base$coefficients[ncol(X)+1]
-  if (!silent) cat('Fitting split loglog...\n')
-  trace <- !silent
-  est <- optim(c(x.inits, rep(0, ncol(Z)), a.init), sploglog_lnlCpp, method="BFGS", 
-    control=list(trace=trace, maxit=max.iter), hessian=T, y=Y, X=X, Z=Z)
-  
-  # Solve other results
-  if (est$convergence!=0 & !silent) stop('Model did not converge')
-  coef <- est$par
-  vcv <- solve(est$hessian)
-  vcv <- make.positive.definite(vcv)
-  logL <- -est$value
-  
-  # Put together results
-  return(list(coefficients = coef, vcv = vcv, logL = logL, base=base))
-}
-
 if(!'loglogTimes.rda' %in% list.files()){
   times=microbenchmark(
     loglog(Y, X, max.iter=200, silent=TRUE),
--- ilc-example/runSpWeib.R
+++ ilc-example/runSpWeib.R
@@ -15,8 +15,11 @@ packs=c("corpcor", 'microbenchmark', 'Rcpp', 'RcppArmadillo', 'ggplot2')
 loadPkg(packs)
 
 # Load core estimation functions
-core_funcs = list.files("../core-functions", pattern="*.R")
-for(func in core_funcs){source(paste0('../core-functions/', func))}
+coreFuncs = list.files("../core-functions", pattern="*.R")
+for(func in coreFuncs){source(paste0('../core-functions/', func))}
+
+cppFuncs = list.files("../core-functions", pattern="*.cpp")
+for(func in cppFuncs){sourceCpp(paste0('../core-functions/', func))}
 
 # Look only at likelihood calculation
 # (Y, X, Z saved from debugging spdur)
@@ -27,66 +30,12 @@ theta <- rnorm(ncol(X) + ncol(Z) + 1)
 
 ################################################################
 # Make sure cpp versions return same result
-sourceCpp("../core-functions/weib_lnlC.cpp")
 weib_lnlCpp(theta, Y, X); weib_lnl(theta, Y, X)
-
-sourceCpp("../core-functions/spweib_lnlC.cpp")
 spweib_lnlCpp(theta, Y, X, Z); spweib_lnl(theta, Y, X, Z)
 ################################################################
 
 ################################################################
 # Speed comparison
-# First we pass cpp likelihood into existing functions from spweibull.R
-#' Regular Weibull regression
-weibullCpp <- function(Y, X, inits=NULL, max.iter, silent=TRUE) {
-  if (is.null(inits)) {
-    inits <- c(rep(0, ncol(X)), 0)
-  }
-  
-  trace <- !silent
-  est <- optim(inits, weib_lnlCpp, method="BFGS", 
-               control=list(trace=trace, maxit=max.iter), 
-               hessian=T, y=Y, X=X)
-  
-  # Solve other results
-  if (est$convergence!=0 & !silent) stop('Model did not converge')
-  coef <- est$par
-  vcv <- solve(est$hessian)
-  vcv <- make.positive.definite(vcv)
-  logL <- -est$value
-  
-  # Put together results
-  return(list(coefficients = coef, vcv = vcv, logL = logL))
-}
-
-# Split Population
-spweibullCpp <- function(Y, X, Z, max.iter, silent=FALSE) {  
-  # Estimate base model
-  if (!exists("base.inits")) {
-    base.inits <- c(rep(0, ncol(X)), 0)
-  }
-  if (!silent) cat('Fitting base weibull...\n')
-  base <- weibull(Y=Y, X=X, inits=base.inits, max.iter=200, silent=TRUE)
-  
-  # Estimate full model
-  x.inits <- base$coefficients[1:ncol(X)]
-  a.init <- base$coefficients[ncol(X)+1]
-  if (!silent) cat('Fitting split weibull...\n')
-  trace <- !silent
-  est <- optim(c(x.inits, rep(0, ncol(Z)), a.init), spweib_lnlCpp, method="BFGS", 
-    control=list(trace=trace, maxit=max.iter), hessian=T, y=Y, X=X, Z=Z)
-  
-  # Solve other results
-  if (est$convergence!=0 & !silent) stop('Model did not converge')
-  coef <- est$par
-  vcv <- solve(est$hessian)
-  vcv <- make.positive.definite(vcv)
-  logL <- -est$value
-  
-  # Put together results
-  return(list(coefficients = coef, vcv = vcv, logL = logL, base=base))
-}
-
 if(!'weibullTimes.rda' %in% list.files()){
   times=microbenchmark(
     weibull(Y, X, max.iter=200, silent=TRUE),
